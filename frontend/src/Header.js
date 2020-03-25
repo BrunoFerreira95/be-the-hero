91044f2e9435ba451c86341c6cee0005a7a9e593
BrunoFerreira95 <brunoferreirapedraca@gmail.com>
2020-03-25 10:37:34 -0300

    deletando arquivos front end

---
+++ frontend/src/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export default function Header(props) {
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+    return (
+        <div>
+            <header>
+                Contador: {counter}
+            </header>
+            <button onClick={increment}>Incrementar</button>
+        </div>
+    );
+}
